6e1c6011314d67c078827282873dcc6f84c64854
Baker, Christopher R <christopher.r.baker@jpmchase.com>
2014-09-21 17:58:06 -0400

    Added parity and abs

--- src/bit_twerking.cpp
+++ src/bit_twerking.cpp
@@ -2,14 +2,43 @@
 // Name        : bit_twerking.cpp
 // Author      : Chris Baker
 // Version     :
-// Copyright   : Your copyright notice
+// Copyright   : None
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
 using namespace std;
 
+int abs(int v) {
+  unsigned int r;  // the result goes here
+  int const mask = v >> sizeof(int) * CHAR_BIT - 1;
+
+  r = (v + mask) ^ mask;
+  return r;
+}
+
+short parity(unsigned long x) {
+  short result = 0;
+  while (x) {
+    printf("x: %ld \n", x);
+    result ^= (x & 1);
+    x >>= 1;
+  }
+  return result;
+}
+
+short parityImproved(unsigned long x) {
+  short result = 0;
+  while (x) {
+    printf("x: %ld \n", x);
+    result ^= 1;
+    x &= (x - 1);
+  }
+  return result;
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+  short b = parity(24);
+  printf("Characters: %d \n", b);
+  return 0;
 }
